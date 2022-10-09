5b47f86ecdafddf89f1d822362557ec6914bdad1
elazar-gadasi <106330969+elazar-gadasi@users.noreply.github.com>
2022-10-09 14:39:19 +0300

    exspress

--- server/app.js
+++ server/app.js
@@ -1,2 +1,12 @@
+const express = require("express");
+const app = express();
+
 const chalk = require("chalk");
 console.log(chalk.green("in app"));
+
+app.get("/", (req, res, next) => {
+  res.send("in listner");
+});
+
+const PORT = 8181;
+app.listen(PORT, () => console.log(chalk.blue(`http://localhost:${PORT}`)));
